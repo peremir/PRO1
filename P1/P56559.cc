8c5b687c40eca36b422f3822b11652133e498156
peremir <57814247+peremir@users.noreply.github.com>
2021-11-18 00:10:53 +0100

    Update P56559.cc

--- P1/P56559.cc
+++ P1/P56559.cc
@@ -7,9 +7,9 @@ using namespace std;
 int main() {
     int a1, b1, a2, b2;
     cin >> a1 >> b1 >> a2 >> b2;
-    if(a1 == a2 && b1 == b2) cout << "=" << endl;
-    else if(a1 >= a2 && b1 <= b2) cout << "1" << endl;
-    else if(a1 <= a2 && b1 >= b2) cout << "2" << endl;
+    if(a1 == a2 and b1 == b2) cout << "=" << endl;
+    else if(a1 >= a2 and b1 <= b2) cout << "1" << endl;
+    else if(a1 <= a2 and b1 >= b2) cout << "2" << endl;
     else cout << "?" << endl;
 }
 
