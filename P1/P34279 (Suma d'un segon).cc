ac83801d175bef0c3687b85c82b9364616a85bcc
peremir <57814247+peremir@users.noreply.github.com>
2021-11-17 23:50:09 +0100

    Update P34279 (Suma d'un segon).cc

--- P1/P34279 (Suma d'un segon).cc
+++ P1/P34279 (Suma d'un segon).cc
@@ -1,1 +1,16 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int h, m, s;
+    cin >> h >> m >> s;
+    s = 1 + s + (m * 60) + (h * 3600);
+    h = (s / 3600) % 24;
+    m = (s % 3600) / 60;
+    s = s % 60;
+    
+    cout << h / 10 << h % 10 << ":" << m / 10 << m % 10 << ":" << s / 10 << s % 10 << endl;
+}
+
+
