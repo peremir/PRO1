33842af3ae4e1db6df12b8d65156f4dd131c6c65
peremir <57814247+peremir@users.noreply.github.com>
2021-11-18 00:03:50 +0100

    Create P89265.cc

---
+++ P1/P89265.cc
@@ -0,0 +1,33 @@
+
+/* Intervals (3) */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a1, b1, a2, b2;
+    cin >> a1 >> b1 >> a2 >> b2;
+    
+    if(a1 == a2 && b1 == b2) cout << "= , ";
+    else if(a1 >= a2 && b1 <= b2) cout << "1 , ";
+    else if(a1 <= a2 && b1 >= b2) cout << "2 , ";
+    else cout << "? , ";
+    
+    if( (a1 <= b2) && (a2 <= b1) ) {
+        if(a1 <= a2 && b1 <= b2) {
+            cout << "[" << a2 << "," << b1 << "]" << endl; 
+        }
+        else if(a1 <= a2 && b1 >= b2) {
+            cout << "[" << a2 << "," << b2 << "]" << endl;
+        }
+        else if(a1 >= a2 && b1 <= b2) {
+            cout << "[" << a1 << "," << b1 << "]" << endl;
+        }
+        else if(a1 <= a2 && b1 >= b2) {
+            cout << "[" << a2 << "," << b2 << "]" << endl;
+        }
+        else cout << "[" << a1 << "," << b2 << "]" << endl;
+    }
+    else cout << "[" << "]" << endl;
+}
+
