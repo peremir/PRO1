efabd347ae295ebd27fd1a3c9901633bade93b24
peremir <57814247+peremir@users.noreply.github.com>
2021-11-18 00:10:02 +0100

    Update P42042.cc

--- P1/P42042.cc
+++ P1/P42042.cc
@@ -9,11 +9,11 @@ int main() {
     cin >> a;
     if(a < 'a') cout << "uppercase" << endl;
     else cout << "lowercase" << endl;
-    if(a == 'a' || a == 'e' || 
-       a == 'i' || a == 'o' || 
-       a == 'u' || a == 'A' || 
-       a == 'E' || a == 'I' || 
-       a == 'O' || a == 'U') {
+    if(a == 'a' or a == 'e' or 
+       a == 'i' or a == 'o' or 
+       a == 'u' or a == 'A' or 
+       a == 'E' or a == 'I' or 
+       a == 'O' or a == 'U') {
         cout << "vowel" << endl;
     }
     else cout << "consonant" << endl;
