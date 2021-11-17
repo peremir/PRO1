8fcdbea6533a7571ecae1250324afe4c6c6928f9
peremir <57814247+peremir@users.noreply.github.com>
2021-11-17 23:56:27 +0100

    Update P92613.cc

--- P1/P92613.cc
+++ P1/P92613.cc
@@ -1,1 +1,17 @@
 
+/* Arrodoniments */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a;
+    cin >> a;
+    cout << int(a) << ' ';
+    double b = a - int(a);
+    if(b == 0) cout << int(a) << ' ';
+    else cout << int(a + 1) << ' ';
+    if(b >= 0.5) cout << int(a + 1) << endl;
+    else cout << int(a) << endl;
+}
+
