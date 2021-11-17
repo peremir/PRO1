0771c5f3ef65c2fa38b5d8b8eee818ab9035aabf
peremir <57814247+peremir@users.noreply.github.com>
2021-11-18 00:12:32 +0100

    Update P89265.cc

--- P1/P89265.cc
+++ P1/P89265.cc
@@ -8,22 +8,22 @@ int main() {
     int a1, b1, a2, b2;
     cin >> a1 >> b1 >> a2 >> b2;
     
-    if(a1 == a2 && b1 == b2) cout << "= , ";
-    else if(a1 >= a2 && b1 <= b2) cout << "1 , ";
-    else if(a1 <= a2 && b1 >= b2) cout << "2 , ";
+    if(a1 == a2 and b1 == b2) cout << "= , ";
+    else if(a1 >= a2 and b1 <= b2) cout << "1 , ";
+    else if(a1 <= a2 and b1 >= b2) cout << "2 , ";
     else cout << "? , ";
     
-    if( (a1 <= b2) && (a2 <= b1) ) {
-        if(a1 <= a2 && b1 <= b2) {
+    if( (a1 <= b2) and (a2 <= b1) ) {
+        if(a1 <= a2 and b1 <= b2) {
             cout << "[" << a2 << "," << b1 << "]" << endl; 
         }
-        else if(a1 <= a2 && b1 >= b2) {
+        else if(a1 <= a2 and b1 >= b2) {
             cout << "[" << a2 << "," << b2 << "]" << endl;
         }
-        else if(a1 >= a2 && b1 <= b2) {
+        else if(a1 >= a2 and b1 <= b2) {
             cout << "[" << a1 << "," << b1 << "]" << endl;
         }
-        else if(a1 <= a2 && b1 >= b2) {
+        else if(a1 <= a2 and b1 >= b2) {
             cout << "[" << a2 << "," << b2 << "]" << endl;
         }
         else cout << "[" << a1 << "," << b2 << "]" << endl;
