ac32afc79926a7d136b569bb9c5ff0835d9c92f0
peremir <57814247+peremir@users.noreply.github.com>
2021-11-17 23:55:30 +0100

    Update P90615.cc

--- P1/P90615.cc
+++ P1/P90615.cc
@@ -1,1 +1,17 @@
 
+/* Màxim de tres enters */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a < b) {
+        if(c < b) cout << b << endl;
+        else cout << c << endl;
+    }
+    else if(c < a) cout << a << endl;
+         else cout << c << endl;
+}
+
