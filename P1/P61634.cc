c9d17316ff39f9a1eff1b2d895200ebfcd15321e
peremir <57814247+peremir@users.noreply.github.com>
2021-11-18 00:07:27 +0100

    Update P61634.cc

--- P1/P61634.cc
+++ P1/P61634.cc
@@ -8,7 +8,7 @@ int main() {
     int y;
     cin >> y;
     if (y % 4 == 0 && y % 100 != 0 || 
-       y % 100 == 0 && (y / 100) % 4 == 0) cout << "YES" << endl;
+        y % 100 == 0 && (y / 100) % 4 == 0) cout << "YES" << endl;
     else cout << "NO" << endl;
 }
 
