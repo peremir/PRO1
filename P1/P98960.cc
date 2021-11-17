c70ad20ac6307202d17498789f7ceecec4b48e25
peremir <57814247+peremir@users.noreply.github.com>
2021-11-17 23:57:15 +0100

    Update P98960.cc

--- P1/P98960.cc
+++ P1/P98960.cc
@@ -1,1 +1,13 @@
 
+/* Majúscules i minúscules */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a;
+    cin >> a;
+    if(a < 'a') cout << char(a + ('a' - 'A')) << endl;
+    else cout << char(a - ('a' - 'A')) << endl;
+}
+
