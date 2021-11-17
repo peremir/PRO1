7ebc0fe9657b413e5396b5cc5e132a86d66336f3
peremir <57814247+peremir@users.noreply.github.com>
2021-11-17 23:59:05 +0100

    Update P42042.cc

--- P1/P42042.cc
+++ P1/P42042.cc
@@ -1,1 +1,17 @@
 
+/* Classificació de caràcters (1) */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a;
+    cin >> a;
+    if(a < 'a') cout << "uppercase" << endl;
+    else cout << "lowercase" << endl;
+    if(a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' || a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U') {
+        cout << "vowel" << endl;
+    }
+    else cout << "consonant" << endl;
+}
+
