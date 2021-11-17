44965e8f635530318ab50bf59fdd2a569f5e9434
peremir <57814247+peremir@users.noreply.github.com>
2021-11-18 00:01:14 +0100

    Update P90226.cc

--- P1/P90226.cc
+++ P1/P90226.cc
@@ -1,1 +1,14 @@
 
+/* Comparació de paraules */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+    if(a > b) cout << a << " > " << b << endl;
+    else if(a < b) cout << a << " < " << b << endl;
+    else cout << a << " = " << b << endl;
+}
+
