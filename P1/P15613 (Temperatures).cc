5914450acf8441ca90e89524614e0a00c253f0ca
peremir <57814247+peremir@users.noreply.github.com>
2021-11-17 23:48:57 +0100

    Update and rename P15613 Temperatures.cc to P15613 (Temperatures).cc

---
+++ P1/P15613 (Temperatures).cc
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    if(t > 30) {
+        cout << "it's hot" << endl;
+        if(t >= 100) cout << "water would boil" << endl;
+    }
+    else if(t < 10) {
+        cout << "it's cold" << endl;
+        if(t <= 0) cout << "water would freeze" << endl; 
+    }
+    else cout << "it's ok" << endl;
+}
+
