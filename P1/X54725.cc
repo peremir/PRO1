969bd04e932f279362830d832d2f815d528a5443
peremir <57814247+peremir@users.noreply.github.com>
2021-11-17 23:54:30 +0100

    Update X54725.cc

--- P1/X54725.cc
+++ P1/X54725.cc
@@ -1,1 +1,13 @@
 
+/* Mínim de dos enters */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if(a < b) cout << a << endl;
+    else cout << b << endl;
+}
+
