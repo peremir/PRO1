3c530670f1cf64cf57ec2835997e1ccca71a2adb
peremir <57814247+peremir@users.noreply.github.com>
2021-11-18 00:04:32 +0100

    Create P48107.cc

---
+++ P1/P48107.cc
@@ -0,0 +1,13 @@
+
+/* Divisió entera i residu de dos naturals */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if(b != 0) cout << (a / b) << " " << (a % b) << endl;
+    else cout << "infinity" << " " << a << endl;
+}
+
