c05a8151b590ba0ffdf2d766bf7dbeed2ab707ce
peremir <57814247+peremir@users.noreply.github.com>
2021-11-18 00:02:27 +0100

    Update P51126.cc

--- P1/P51126.cc
+++ P1/P51126.cc
@@ -1,1 +1,27 @@
 
+/* Intervals (1) */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a1, b1, a2, b2;
+    cin >> a1 >> b1 >> a2 >> b2;
+    if( (a1 <= b2) && (a2 <= b1) ) {
+        if(a1 <= a2 && b1 <= b2) {
+            cout << "[" << a2 << "," << b1 << "]" << endl; 
+        }
+        else if(a1 <= a2 && b1 >= b2) {
+            cout << "[" << a2 << "," << b2 << "]" << endl;
+        }
+        else if(a1 >= a2 && b1 <= b2) {
+            cout << "[" << a1 << "," << b1 << "]" << endl;
+        }
+        else if(a1 <= a2 && b1 >= b2) {
+            cout << "[" << a2 << "," << b2 << "]" << endl;
+        }
+        else cout << "[" << a1 << "," << b2 << "]" << endl;
+    }
+    else cout << "[" << "]" << endl;
+}
+
